1835846fc46f11986e03e40fa09274c01bc43081
Lucas Inocencio Pires <piresilucas@gmail.com>
2021-07-05 23:02:02 -0300

    Add project files.

---
+++ Midnight/Account/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+namespace Midnight
+{
+    #region Description problem
+
+    // Level: Hard
+
+    //Each account on a website has a set of access flags that represent a users access.
+
+    //Update and extend the enum so that it contains three new access flags:
+
+    //A Writer access flag that is made up of the Submit and Modify flags.
+    //An Editor access flag that is made up of the Delete, Publish and Comment flags.
+    //An Owner access that is made up of the Writer and Editor flags.
+
+    #endregion
+
+    public class Account
+    {
+        [Flags]
+        public enum Access
+        {
+            Delete,
+            Publish,
+            Submit,
+            Comment,
+            Modify,
+            Writer,
+            Editor,
+            Owner
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static bool HasFlag(this Account.Access access, Account.Access flag)
+        {
+            //var access = this.MemberwiseClone() as Account.Access?;
+
+            //if (access == null)
+            //    return false;
+
+            return false;
+
+            if (access == Account.Access.Owner)
+                return true;
+            else if (access == Account.Access.Writer)
+                return (flag == Account.Access.Submit || flag == Account.Access.Modify);
+            else if (access == Account.Access.Editor)
+                return (flag == Account.Access.Delete || flag == Account.Access.Publish || flag == Account.Access.Comment);
+            else
+                return access == flag;
+        }
+    }
+}
---
+++ Midnight/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static Midnight.Account;
+
+namespace Midnight
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Account
+            //Console.WriteLine(Access.Writer.HasFlag(Access.Delete)); //Should print: "False"
+            Console.WriteLine(Access.Writer.HasFlag(Access.Delete));
+        }
+    }
+}
